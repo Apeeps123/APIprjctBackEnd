d16ccbe76e7fc4a2361775bce5077916b2e8f14e
Apeeps123 <afifversa123@gmail.com>
2023-09-19 16:05:00 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000 
+
+const bodyPs = require('body-parser'); 
+app.use(bodyPs.urlencoded({ extended: false}));
+app.use(bodyPs.json());
+
+const mhsRouter = require('./routes/mahasiswa');
+app.use('/api/mhs', mhsRouter);
+
+app.listen(port, () => {
+    console.log(`running app http:://localhost:${port}`)
+})
